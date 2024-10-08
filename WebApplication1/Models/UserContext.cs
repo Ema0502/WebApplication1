828fcf02ab0d490b1d5aeee753ca561b98bc56fa
Ema0502 <ema050296@gmail.com>
2024-10-08 00:08:33 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication1/Models/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace YourApi.Models
+{
+  public class UserContext : DbContext
+  {
+    public UserContext(DbContextOptions<UserContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; } = null!;
+  }
+}
